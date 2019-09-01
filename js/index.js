a797aa308eecf3e7d8d2c6dd1efd976d3c281e10
nysar.yam <nysar.yam@PNC.LAN>
2019-09-01 17:28:14 +0700

    fuction

---
+++ js/index.js
@@ -0,0 +1,30 @@
+
+function resizeButtonPeole() {
+  let width = 0;
+  let buttons = document.getElementsByClassName('button-people');
+
+  for (let button of buttons){
+    button.style.width = "auto";
+  }
+
+  for (let button of buttons){
+    if(button.offsetWidth > width){
+      width = button.offsetWidth;
+    }
+  }
+
+  for(let button of buttons){
+    button.style.width = width + 2 +"px";
+      button.style.width = `${width + 2}px`;
+  }
+}
+
+
+
+
+
+
+window.onload = function(){
+  resizeButtonPeole();
+
+}
